804e81f3bdde38ced4cc9f6385a576dd58a69c64
Álvaro <lvaro@DESKTOP-LJIOVA9>
2023-02-01 20:30:11 +0100

    Líneas en el método main.

---
+++ src/controldeversiones/Principal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controldeversiones;
+
+/**
+ *
+ * @author Álvaro
+ */
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Primera línea de visualización del ejercicio.");
+        System.out.println("Segunda línea de visualización del ejercicio.");
+    }
+}
